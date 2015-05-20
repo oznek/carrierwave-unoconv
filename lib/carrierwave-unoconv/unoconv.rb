b027a402ebf40480f2782d2182b9d9a4d13ffcc6
Kenzo Okamura <kenzo.okamura@gmail.com>
2015-05-20 04:15:08 -0300

    Added unoconv filter options parameter.
    - Allows unoconv export filter options to be passed
      https://wiki.openoffice.org/wiki/Documentation/DevGuide/Spreadsheets/Filter_Options

--- lib/carrierwave-unoconv/unoconv.rb
+++ lib/carrierwave-unoconv/unoconv.rb
@@ -2,21 +2,24 @@ module CarrierWave
   module UNOConv
     extend ActiveSupport::Concern
     module ClassMethods
-      def uno_convert format
-        process uno_convert: format
+      def uno_convert(format, opts={})
+        process uno_convert: [format, opts]
       end
     end
-  
-    def uno_convert format
+
+    def uno_convert(format, opts={})
       directory = File.dirname( current_path )
       tmpfile   = File.join( directory, "tmpfile" )
-  
+
+      filter_options = opts[:filter_options]
+      filter_options = "-e FilterOptions=#{filter_options}" unless filter_options.blank?
+
       File.rename( current_path, tmpfile )
-        
-      raise CarrierWave::ProcessingError, "UNOconv failed." unless system "unoconv -f #{format} '#{tmpfile}'"
-      
+
+      raise CarrierWave::ProcessingError, "UNOconv failed." unless system "unoconv -f #{format} #{filter_options} '#{tmpfile}'"
+
       File.rename( File.join(directory, "tmpfile.#{format}"), current_path )
-  
+
       File.delete( tmpfile )
     end
   end
